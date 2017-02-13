7fcb98c631b9aa86617b766ebd0a5fb8f63b5fa4
Benjamin Wolf <bwolfgns@gmail.com>
2017-02-12 23:43:40 -0500

    Added method to read user code

--- src/text-editor.js
+++ src/text-editor.js
@@ -1,3 +1,9 @@
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
 editor.session.setMode("ace/mode/javascript");
+editor.getSession().setValue("Your code here");
+
+var input = $('input[name="code"]');
+        editor.getSession().on("change", function() {
+        input.val(editor.getSession().getValue());
+    });
