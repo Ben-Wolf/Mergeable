8e0d2321f6120881336e8b57cb8ff7752b635346
Benjamin Wolf <bwolfgns@gmail.com>
2017-02-28 02:54:47 -0500

    Added Options for theme and languages

--- src/assets/js/text-editor.js
+++ src/assets/js/text-editor.js
@@ -1,15 +1,40 @@
 var editor = ace.edit("editor");
 
-editor.setTheme("ace/theme/monokai");
-editor.session.setMode("ace/mode/javascript");
-editor.getSession().setValue("Your code here");
-
 var input = $('input[name="code"]');
-    editor.getSession().on("change", function() {
-    input.val(editor.getSession().getValue());
+  editor.getSession().on("change", function() {
+  input.val(editor.getSession().getValue());
 });
 
 editor.getSession().on('change', function(e) {
 
 
+});
+
+$(document).ready(function() {
+
+  editor.setTheme("ace/theme/monokai");
+  editor.session.setMode("ace/mode/javascript");
+  editor.getSession().setValue("Your code here");
+
+  /* Language Options */
+  $("html").click(function() {
+    editor.session.setMode("ace/mode/html");
+  });
+  $("css").click(function() {
+    editor.session.setMode("ace/mode/css");
+  });
+  $("javascript").click(function() {
+    editor.session.setMode("ace/mode/javascript");
+  });
+
+  /* Theme Options */
+  $("#eclipse").click(function() {
+    editor.setTheme("ace/theme/eclipse");
+  });
+  $("#cobalt").click(function() {
+    editor.setTheme("ace/theme/cobalt");
+  });
+  $("#monokai").click(function() {
+    editor.setTheme("ace/theme/monokai");
+  });
 });
