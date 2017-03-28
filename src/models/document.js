54866926e4cb8f69c54eac5bebaf1af8a5ce9baf
Eva Jiang <evajiang322@gmail.com>
2017-03-28 12:15:49 -0400

    created document schema

--- src/models/document.js
+++ src/models/document.js
@@ -1,3 +1,22 @@
 var mongoose = require('mongoose');
 
 // Document Schema
+var documentSchema = mongoose.Schema ({
+  owner: {
+    type: String
+  },
+  dateCreated: {
+    type: Date
+  },
+  lastModified: {
+    type: Date
+  },
+  file: {
+    type: String
+  },
+  otherEditors: {
+    type: [String]
+  }
+});
+
+var Document = module.exports = mongoose.model('Document', documentSchema);
